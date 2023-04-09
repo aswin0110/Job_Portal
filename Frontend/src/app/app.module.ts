db1a76db06a1a0ac2ec404eb3ce4cb79547221f7
Aswin <106148294+aswin0110@users.noreply.github.com>
2023-04-09 14:31:40 +0530

    first commit: navbar,home

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
