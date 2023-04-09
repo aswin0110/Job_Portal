a3df836dd0bf0b5953d61d1d208a37105dec21ba
Aswin <106148294+aswin0110@users.noreply.github.com>
2023-04-10 00:53:43 +0530

    change navbar folder to components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './pages/navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LoginpageComponent } from './pages/loginpage/loginpage.component';
 import { FooterComponent } from './components/footer/footer.component';
