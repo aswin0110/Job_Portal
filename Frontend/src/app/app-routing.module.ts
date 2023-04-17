71d0e09e9958f128f1363c5e5e569c553d2981b8
Aswin <106148294+aswin0110@users.noreply.github.com>
2023-04-17 09:13:20 +0530

    home changes

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LoginpageComponent } from './pages/loginpage/loginpage.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { CareerComponent } from './pages/career/career.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 
 const routes: Routes = [
   {path:'', component:HomepageComponent},
   {path:'login', component:LoginpageComponent},
-  {path:'signup', component:SignupComponent}
+  {path:'signup', component:SignupComponent},
+  {path:'contactus', component:ContactusComponent},
+  {path:'career', component:CareerComponent},
+  {path:'aboutus', component:AboutusComponent}
 ];
 
 @NgModule({
