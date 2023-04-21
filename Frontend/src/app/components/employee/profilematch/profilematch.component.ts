5e6edbf1f9c61daf4049bff8a1dc89acfce495fa
Aswin <106148294+aswin0110@users.noreply.github.com>
2023-04-21 15:54:08 +0530

    profilematch: template added

--- Frontend/src/app/components/employee/profilematch/profilematch.component.ts
+++ Frontend/src/app/components/employee/profilematch/profilematch.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profilematch',
-  templateUrl: './profilematch.component.html',
+  templateUrl:'./profilematch.component.html',
   styleUrls: ['./profilematch.component.css']
 })
 export class ProfilematchComponent {
