8c2b182dbb6ebd1af878848afdacd0a91f583272
Aparna <aparnaraji2000@gmail.com>
2023-04-30 12:02:04 +0530

    no

--- Backend/routes/manageemployee.js
+++ Backend/routes/manageemployee.js
@@ -1,34 +1,33 @@
-const express = require('express')
-const router = express.Router()
+// const express = require('express')
+// const router = express.Router()
 
-const manageData = require('../model/manageemployeeModel')
+// const manageData = require('../model/manageemployeeModel')
 
-router.put('/api/manageemployee/:id' , async(req,res)=>{
+// router.put('/api/manageemployee/:id' , async(req,res)=>{
     
-    try {
+//     try {
         
-        let id = req.params.id
+//         let id = req.params.id
 
-        let item = {  //to fetch and save data from frontend in server
-            employername : req.body.employername,
-            designation : req.body.designation,
-            email : req.body.email,
-            phonenumber:req.body.phonenumber
-         }
+//         let item = {  //to fetch and save data from frontend in server
+//             employername : req.body.employername,
+//             designation : req.body.designation,
+//             email : req.body.email,
+//             phonenumber:req.body.phonenumber
+//          }
          
-        //  let token = req.headers.authorization
-        //  console.log('token from front end',token) 
-         const updateData = { $set :item }
+      
+//          const updateData = { $set :item }
 
-        const updateEmployee = await manageData.findByIdAndUpdate( id, updateData)
-        res.json({message :'Data updated succesfully'}).status(200)
-        res.send(updateEmployee)
+//         const updateEmployee = await manageData.findByIdAndUpdate( id, updateData)
+//         res.json({message :'Data updated succesfully'}).status(200)
+//         res.send(updateEmployee)
    
-    } catch (error) {
-        console.log(error)
-        res.json({message:error}).status(400) 
-    }
-})
+//     } catch (error) {
+//         console.log(error)
+//         res.json({message:error}).status(400) 
+//     }
+// })
 
 
 
@@ -38,4 +37,4 @@ router.put('/api/manageemployee/:id' , async(req,res)=>{
 
 
 
-module.exports = router
+// module.exports = router
