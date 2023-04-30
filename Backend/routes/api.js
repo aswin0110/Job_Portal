51f512a8b18734fff13480f0dda42143c1e95ab7
Aparna <aparnaraji2000@gmail.com>
2023-04-30 18:48:49 +0530

    no

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // signup
-router.use('/alumnisignup',require('./alumnisignup'))
+router.use('/signup',require('./alumnisignup'))
 
 // employer signup
 router.use('/employersignup',require('./employersignup'))
@@ -25,7 +25,7 @@ router.use('/data/alumni', require('./admindashboard'))
 router.use('/jobpost', require('./jobform'))
 
 // manageemployee
-router.use('/manageemployee', require('./manageemployee'))
+// router.use('/manageemployee', require('./manageemployee'))
 
 
 
